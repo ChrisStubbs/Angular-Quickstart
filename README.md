76b0101951f3e7616b75a450e6f691808f8b852c
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-05-22 22:14:56 +0100

    Services chapter

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Angular QuickStart 
 
-[Current place Part 3](https://angular.io/docs/ts/latest/tutorial/toh-pt3.html)
+Start Of The routing section for the next lesson
+[Current place Part 5](https://angular.io/docs/ts/latest/tutorial/toh-pt4.html)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -75,13 +76,24 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[]
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
